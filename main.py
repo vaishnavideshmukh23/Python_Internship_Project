73c9e05f2221e22f77d1fd6751963167f93eac24
Vaishnavi Deshmukh <vaishnavideshmukh463@gmail.com>
2026-05-26 18:21:39 +0530

    Completed the Guess the Number Game project

---
+++ main.py
@@ -0,0 +1,38 @@
+print('''
+                         | |                  
+ _ __  _   _ _ __ ___ | |__   ___ _ __ ___ 
+| '_ \| | | | '_ ` _ \| '_ \ / _ \ '__/ __|
+| | | | |_| | | | | | | |_) |  __/ |  \__ \
+|_| |_|\__,_|_| |_| |_|_.__/ \___|_|  |___/
+''')
+
+#Welcome to the "Number Guessing Game" !!
+import random
+print("WELCOME TO THE GUESS THE NUBMER GAME !")
+print("Guess the number between 1 to 10 ?")
+#Applied outer loop
+while True:
+    secret_number = (random.randint(1, 100))
+    attempts = 3
+    win = False
+    #Inner loop
+    while attempts > 0:
+        #Gives input to the user
+        user_guess = int(input("Enter your guess ?"))
+        #Applied if else statement
+        if user_guess == secret_number:
+            print("Correct!! You guessed the number.")
+        else:
+            attempts -= 1
+            print("Wrong")
+            #Applied nesetled if statement
+            if attempts > 0:
+                print("Attempts left:", attempts)
+    if not win and attempts == 0:
+        print("Game Over ! The number was :", secret_number)
+    again = input("Do you want to play again? (yes/no): ").lower()
+    if again != "yes":
+        print("Thanks for playing")
+        break
+
+
